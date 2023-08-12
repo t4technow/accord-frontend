a9991a14ca042d930ce89ed9343d6948b81d9da8
Mohammed Murshid <t4technow@gmail.com>
2023-08-12 17:17:31 +0530

    production config

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,6 +1,6 @@
-export const baseUrl = "http://moddroid.tk/api/";
-export const imageBase = "http://moddroid.tk";
-export const wsHead = 'ws://moddroid.tk/'
+export const baseUrl = "https://moddroid.tk/api/";
+export const imageBase = "https://moddroid.tk";
+export const wsHead = 'wss://moddroid.tk/'
 
 // export const baseUrl = "http://192.168.43.76:5555/api/";
 // export const imageBase = "http://192.168.43.76:5555";
