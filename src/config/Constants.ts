f223c1630ab1e4b3153f495900d548accc1278e4
Mohammed Murshid <t4technow@gmail.com>
2023-08-15 16:27:58 +0530

    server config

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,7 +1,7 @@
-export const baseUrl = "https://moddroid.tk/api/";
-export const imageBase = "https://moddroid.tk/api";
-export const wsHead = 'wss://moddroid.tk/api/'
+// export const baseUrl = "http://127.0.0.1:5555/api/";
+// export const imageBase = "http://127.0.0.1:5555";
+// export const wsHead = 'ws://127.0.0.1:5555/'
 
-// export const baseUrl = "http://192.168.43.76:5555/api/";
-// export const imageBase = "http://192.168.43.76:5555";
-// export const wsHead = 'ws://192.168.43.76:5555/'
+export const baseUrl = "https://accord-api/api/";
+export const imageBase = "https://accord-api";
+export const wsHead = 'wss://accord-api/'
