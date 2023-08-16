ec7dd58b4ef1fa2215d6b0b064f174e941cd1555
Mohammed Murshid <t4technow@gmail.com>
2023-08-16 10:45:39 +0530

    reverted SW

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,7 +1,7 @@
-// export const baseUrl = "http://127.0.0.1:5555/api/";
-// export const imageBase = "http://127.0.0.1:5555";
-// export const wsHead = 'ws://127.0.0.1:5555/'
+export const baseUrl = "http://127.0.0.1:5555/api/";
+export const imageBase = "http://127.0.0.1:5555";
+export const wsHead = 'ws://127.0.0.1:5555/'
 
-export const baseUrl = "https://accord-api.t4technow.com/api/";
-export const imageBase = "https://accord-api.t4technow.com";
-export const wsHead = 'wss://accord-api.t4technow.com/'
+// export const baseUrl = "https://accord-api.t4technow.com/api/";
+// export const imageBase = "https://accord-api.t4technow.com";
+// export const wsHead = 'wss://accord-api.t4technow.com/'
