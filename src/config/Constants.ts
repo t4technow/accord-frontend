684c19431e434fd21218f8aa6824476b6516e354
Mohammed Murshid <t4technow@gmail.com>
2023-08-19 17:05:21 +0530

    fixed some styling issues

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,9 +1,9 @@
-export const baseUrl = "http://127.0.0.1:5555/api/";
-export const imageBase = "http://127.0.0.1:5555";
-export const wsHead = 'ws://127.0.0.1:5555/'
+// export const baseUrl = "http://127.0.0.1:5555/api/";
+// export const imageBase = "http://127.0.0.1:5555";
+// export const wsHead = 'ws://127.0.0.1:5555/'
 
 export const VAPID_PUBLIC_KEY = 'BK95jBHi_8FQsytb4NIZB3XOtPnoc_LPDPikuDGIb_SJUE5bR_vslHBedZM1Mfbb1smR6YdbFCCsgQlhlmYatyU'
 
-// export const baseUrl = "https://accord-api.t4technow.com/api/";
-// export const imageBase = "https://accord-api.t4technow.com";
-// export const wsHead = 'wss://accord-api.t4technow.com/'
+export const baseUrl = "https://accord-api.t4technow.com/api/";
+export const imageBase = "https://accord-api.t4technow.com";
+export const wsHead = 'wss://accord-api.t4technow.com/'
