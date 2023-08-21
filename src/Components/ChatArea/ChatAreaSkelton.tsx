be21f324d3f2917d5ced53924f87435026c6d395
Mohammed Murshid <t4technow@gmail.com>
2023-08-21 14:42:03 +0530

    send messages

--- src/Components/ChatArea/ChatAreaSkelton.tsx
+++ src/Components/ChatArea/ChatAreaSkelton.tsx
@@ -2,7 +2,31 @@ import './ChatArea.css'
 const ChatAreaSkelton = () => {
   return (
     <div className="chat-area">
-
+      <li className="skelton-list_item">
+        <div className='online-indicator online'></div>
+        <div className="skelton-avatar" ></div>
+        <span className="skeleton skeleton-text"></span>
+      </li>
+      <li className="skelton-list_item">
+        <div className='online-indicator online'></div>
+        <div className="skelton-avatar" ></div>
+        <span className="skeleton skeleton-text"></span>
+      </li>
+      <li className="skelton-list_item">
+        <div className='online-indicator online'></div>
+        <div className="skelton-avatar" ></div>
+        <span className="skeleton skeleton-text"></span>
+      </li>
+      <li className="skelton-list_item">
+        <div className='online-indicator online'></div>
+        <div className="skelton-avatar" ></div>
+        <span className="skeleton skeleton-text"></span>
+      </li>
+      <li className="skelton-list_item">
+        <div className='online-indicator online'></div>
+        <div className="skelton-avatar" ></div>
+        <span className="skeleton skeleton-text"></span>
+      </li>
     </div>
   )
 }
